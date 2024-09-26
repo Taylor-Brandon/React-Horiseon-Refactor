5294c2610372d24ee0f64d281bffa40272614bc5
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-09-26 13:04:40 -0400

    added header component and styled; added style folder and file; added bootstrap

--- horiseon-refactor/src/App.js
+++ horiseon-refactor/src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import HelloReact from './components/HelloReact';
+import Header from './components/header';
+import './styles/style.css';
 
-// TODO: Add a comment explaining what this function is doing
-// This function is a functional component that helps us split the UI into distinct parts.
-// In this case, we are returning another component, <HelloReact/> from it.
-function App() {
-  return <HelloReact />;
+export default function App() {
+  return(
+    <Header />
+  );
 }
-
-export default App;
