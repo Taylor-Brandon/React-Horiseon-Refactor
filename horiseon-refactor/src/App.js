d56dc7f5ee462710e91a9bad4eb2835a74dbf3d7
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-09-27 13:32:33 -0400

    added main img to app.js, added styling, added img to new images folder

--- horiseon-refactor/src/App.js
+++ horiseon-refactor/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from './components/header';
+import Img from './images/main.jpg';
 import './styles/style.css';
 
 export default function App() {
   return(
+    <div>
     <Header />
+    <img className="hero" src={Img} alt="digital marketing meeting img"></img>
+    </div>
   );
 }
