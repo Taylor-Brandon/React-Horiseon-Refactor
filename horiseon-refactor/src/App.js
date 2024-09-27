2e16245c5fe1f830edcd798eea15bc7ea62ccdef
Taylor-Brandon <117056023+Taylor-Brandon@users.noreply.github.com>
2024-09-27 13:47:34 -0400

    added search optimization component, styling, and img with styling and added to app.js

--- horiseon-refactor/src/App.js
+++ horiseon-refactor/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import Header from './components/header';
+import Header from './components/header/header';
 import Img from './images/main.jpg';
+import Search from './components/searchOpt/search';
 import './styles/style.css';
 
 export default function App() {
@@ -8,6 +9,9 @@ export default function App() {
     <div>
     <Header />
     <img className="hero" src={Img} alt="digital marketing meeting img"></img>
+    <div className='main'>
+    <Search />
+    </div>
     </div>
   );
 }
---
+++ horiseon-refactor/src/components/searchOpt/search.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import SearchImg from '../../images/search.jpg';
+
+
+export default function Search() {
+    return(
+       <div className='Search-Optimization'>
+        <div class="content-container">
+        <section class="content">
+            <div class="article">
+                <img id='search-img' src={SearchImg} class="float-left" alt="Notebook with SEO text Img" />
+                <h2>Search Engine Optimization</h2>
+                <p>The dominance of mobile internet use means that users are searching for the right business as they
+                    travel, shop, or sit on their couch at home. Search Engine Optimization (SEO) allows you to increase
+                    your visibility and find the right customers for your business.</p>
+                    </div>
+            </section>
+        </div>
+    </div>
+    );
+}
